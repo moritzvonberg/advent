d2c241d0aa5603c294710fa67432d16a1fd055bf
Moritz von Berg <moritzvonberg@gmail.com>
2023-01-03 07:43:54 +0100

    day 4 part 2

---
+++ 2022/day 4/clean2.py
@@ -0,0 +1,25 @@
+with open("2022/day 4/input.txt") as infile:
+    assignment_pairs = [x.strip().split(',') for x in infile.readlines()]
+
+count = 0
+
+for i, assignment_pair in enumerate(assignment_pairs):
+    pair = []
+    for assignment in assignment_pair:
+        low, high = assignment.split('-')
+        pair.append((int(low), int(high)))
+    assignment_pairs[i] = pair
+
+def fully_overlap(range1: int, range2: int) -> bool:
+    return (range1[0] <= range2[0] and range1[1] >= range2[1]) or (range1[0] >= range2[0] and range1[1] <= range2[1])
+
+def partially_overlap(range1: int, range2: int) -> bool:
+    return (range1[0] >= range2[0] and range1[0] <= range2[1]) or (range1[1] >= range2[0] and range1[1] <= range2[1])
+
+for range1, range2 in assignment_pairs:
+    if partially_overlap(range1, range2) or fully_overlap(range1, range2):
+        print(f"range1:{range1}, range2:{range2}")
+        count += 1
+    
+
+print(count)
