c5e7240a94f0e710bcc3804315aa8bdebae64ea6
Moritz von Berg <moritzvonberg@gmail.com>
2023-12-01 07:43:02 +0100

    cleanup

--- 2023/day 1/solver.py
+++ 2023/day 1/solver.py
@@ -5,20 +5,20 @@ from aocd import get_data, submit
 
 data = get_data()
 
-
 words = [
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
 ]
-res = 0
 
+res = 0
 for line in data.splitlines():
     found_nums = []
     for i, char in enumerate(line):
         if char.isdigit():
             found_nums.append((i, char))
     for i, word in enumerate(words):
+        word_value = str(i + 1)
         for match in re.finditer(word, line):
-            found_nums.append((match.start(), str(i + 1)))
+            found_nums.append((match.start(), word_value))
 
     found_nums.sort(key=itemgetter(0))
 
